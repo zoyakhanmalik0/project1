bf7596a39e2cf08f61cdeb14c978ca9f7ae39aec
MuzamilElahi <muzamilelahi01@gmail.com>
2025-07-23 11:55:03 +0500

    Update base and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // <-- IMPORTANT
+  base: '/project1/', // ✅ Important: Match your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
